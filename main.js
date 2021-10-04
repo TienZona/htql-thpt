3b78e50df401cdbe8fcb7b446d8c3e538565a9a8
tienzona001 <tienzona001@gmail.com>
2021-10-04 11:30:12 +0700

    update file

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-var accounts  = [
+const accounts  = [
     {
         id: 1,
         name: 'TienZona',
+        fullname: 'Chung Phát Tiến',
         password: '123456',
         position: 'boss'
     },
     {
         id: 2,
         name: 'HieuTruong',
+        fullname: 'Thầy hiệu trưởng',
         password: '123456',
         position: 'Administrators'
     },
     {
         id: 3,
         name: 'GiaoVienBoMon',
+        fullname: 'Giáo viên bộ môn',
         password: '123456',
         position: 'teacher-subject'
     },
     {
-        id: 3,
+        id: 4,
         name: 'GiaoVienChuNhiem',
+        fullname: 'Giáo viên chủ nhiệm',
         password: '123456',
         position: 'teacher-homeroom'
     }
@@ -27,30 +31,125 @@ var accounts  = [
 
 
 
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-var btnOut = $('.navbar__menu-btn--out');
+const btnOut = $('.navbar__menu-btn--out');
+const userName = $('.navbar__menu-user');
+const outBtn = $('.navbar__menu-btn--out');
+const homeBtn = $('.navbar__menu-btn--home');
+const itemDecen = $('#list-items--decen');
+const containerList = $('.container__list');
+const containerDecen = $('.container__decen');
+
 
 if(btnOut){
     btnOut.onclick = function(e){
         alert('Bạn có chắc muốn thoát');
-        window.location.href = "./login.html";
+        display(containerList,false);
+        display(form,true);
+        display(outBtn,false);
+        display(userName,false);
+        display(homeBtn,false);
+        display(containerDecen, false);
     }
 }
 
-var homeIndex = function(){
-    window.location.href = "./index.html";
+function getUser(user){
+    var user = accounts.filter(function(account){
+        return user.fullname === account.name;
+    })
+    return user[0];
 }
 
-
-var getAccount = function() {
+function getAccount() {
     return accounts;
 }
 
-var checkAccount = function(inputAccount){
-    var accounts = getAccount();
+function checkAccount(inputAccount){
+    const accounts = getAccount();
     return accounts.some(function(account){
         if((inputAccount.fullname === account.name) && (inputAccount.password === account.password)) return true;
     });
 }
+
+if(itemDecen) {
+    itemDecen.onclick = function(e){
+        containerDecen.classList.remove('display--none');
+        containerList.classList.add('display--none');
+        renderDecen(accounts);
+    }
+}
+
+function display(elementName, isDisplay){
+    var element
+    if(typeof elementName === 'string'){
+        element = $(elementName);
+    }else{
+        element = elementName;
+    }
+    if(isDisplay){
+        element.classList.remove('display--none');
+    }else {
+        element.classList.add('display--none');
+    }
+}
+
+function logged(data){
+    containerList.classList.remove('display--none');
+    form.classList.add('display--none');
+    userName.innerHTML = `${getUser(data).fullname}`;
+    outBtn.classList.remove('display--none');
+    userName.classList.remove('display--none');
+    display(homeBtn, true);
+    if(homeBtn){
+        homeBtn.onclick = function(e){
+            display('.container__list',true);
+            display('.container__decen', false);
+        }
+    }   
+}
+
+// Phân quyền hệ thống
+const decentralization = $('#container__decen');
+const decenList = $('.decen__list');
+
+function renderDecen(accounts){
+    const htmls = accounts.map(function(account, index){
+        return `
+        <div class="decen__list-users">
+            <img class="users__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQctA-NSI59h7c-JFVTUWhozUanBiB-rX0MMg&usqp=CAU" alt="">
+            <table class="users__infor">
+                <tr>
+                    <th class="users__infor-name">${account.fullname}</th>
+                </tr>
+                <tr>
+                    <td class="users__infor-position">Chức vụ: ${account.position}</td>
+                </tr>
+            </table>
+            <div class="users__edit">
+                <label for="position" class="users__label">Chỉnh sửa: </label>
+                <select name="position" id="position" class="users__control">
+                    <option value="">Lựa chọn chức vụ</option>
+                    <option value="Administrators">Administrators</option>
+                    <option value="teacher-subject">teacher-subject</option>
+                    <option value="teacher-homeroom">teacher-homeroom</option>
+                </select>
+                <button class="users__edt-btn">Lưu</button>
+            </div>
+        </div>
+        `
+    })
+    decenList.innerHTML = htmls.join('');
+    const editBtns = $$('.users__edt-btn');
+    Array.from(editBtns).forEach(function(editBtn,index){
+        editBtn.onclick = function(){
+            const position = this.parentElement.parentElement.querySelector('.users__infor-position');
+            const valueEdit = this.parentElement.querySelector('.users__control').value;
+            if(valueEdit != ''){
+                position.innerHTML = `Chức vụ: ${valueEdit}`;
+            }
+        } 
+    })
+}
+
